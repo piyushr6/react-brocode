7dcad2d0330358108c0363858fdd727922611de8
Piyush Rathi <piyushrathi06@gmail.com>
2025-01-26 14:50:11 +0530

    2. Card and started with 3. styling css

---
+++ react-course/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+
+   return (
+      <div className="card">
+         <img className="card-image" src="https://media.licdn.com/dms/image/v2/D4D03AQFGNQiy6-ynMA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1702400005707?e=2147483647&v=beta&t=JRC55AwimA66Z74muURGHWjZC31ipC_Ez1w3d_R9gF4" alt="pfp" />
+         <h2 className="card-title">Piyush</h2>
+         <p className="card-text">SE Comps at SPIT</p>
+      </div>
+   )
+}
+
+export default Card
