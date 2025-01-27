135fa8f43bc81f5af17ab6fe428bbd521ffe8e1d
Piyush Rathi <piyushrathi06@gmail.com>
2025-01-28 00:12:33 +0530

    4. props completed

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,15 +1,34 @@
-
-//STYLING COMPONENTS CSS IN 3 WAYS - EXTERNAL, MODULES, INLINE  
-import Button from './Button/Button.jsx';
+import Student from './Student.jsx';
 
 function App() {
 
   return (
-    <Button />
+    <>
+      <Student name="Spongebob" age={18} isStudent={true} />
+      <Student name="Rick" age={20} isStudent={false} />
+      <Student name="Dazel" age={22} isStudent={true} />
+      <Student name="ABC" age={21} isStudent={false} />
+      <Student name="XYZ" age={23} isStudent={false} />
+      <Student />
+    </>
   );
 }
 
 export default App
+
+
+
+//STYLING COMPONENTS CSS IN 3 WAYS - EXTERNAL, MODULES, INLINE  
+// import Button from './Button/Button.jsx';
+
+// function App() {
+
+//   return (
+//     <Button />
+//   );
+// }
+
+// export default App
 
 
 
